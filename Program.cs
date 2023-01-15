206b22a9728db2d4b861bd85ef509a017af46a98
Roman <rtkcfc@gmail.com>
2023-01-15 16:49:02 +0600

    Firsttry

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.Write("Введите первое число: ");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        System.Console.Write("Введите второе число: ");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+        
+        
+        if (firstNumber<secondNumber)
+        {
+            System.Console.WriteLine($"Число {secondNumber} больше чем {firstNumber}");
+        }
+        else if (firstNumber>secondNumber)
+        {
+            System.Console.WriteLine($"Число {firstNumber} больше чем {secondNumber}");
+        }
+    }
+}
